09bf1c49c9fab98c084a129058f8acd8ee154dee
TRAN Xuan Tien <txtien@gmail.com>
2019-04-29 07:03:10 +0200

    Encore ORM

---
+++ MVCdatabasefirst/Controllers/Default1Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCdatabasefirst.Models;
+
+namespace MVCdatabasefirst.Controllers
+{
+	public class Default1Controller : Controller
+	{
+		// GET: Default1
+		public ActionResult Index()
+		{
+			using (var l = new EmployeeDBEntities())
+			{
+				IList<EmployeeDetails> employees = l.EmployeeDetails.ToList();
+				return View(employees);
+			}
+		}
+	}
+}
